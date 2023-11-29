43699b01f5c05088fd1fcc25073cce2f1e090c52
OnahNeLson <Onahnelson2@gmail.com>
2023-11-29 05:02:43 +0100

    worker file

--- worker.js
+++ worker.js
@@ -4,6 +4,7 @@ import { promises as fs } from 'fs';
 import { ObjectID } from 'mongodb';
 import dbClient from './utils/db';
 
+
 const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
 const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
